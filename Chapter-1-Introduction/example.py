e77d2c4d8506abfdc051e9148a5aa6ee127ab5e6
howardcheung <howard.at@gmail.com>
2016-11-19 17:22:58 +0800

    Completing the introduction section

---
+++ Chapter-1-Introduction/example.py
@@ -0,0 +1,19 @@
+#!/bin/usr/python3
+"""
+    Created to demonstrate the usefuleness of Python with Pandas
+    and Matplotlib
+
+    Author: Howard Cheung
+    Date: 2016/11/19
+"""
+
+
+from datetime import datatime, timedelta
+
+
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    
